68bb48d3c336f838ed4c75d0fcc989953a912631
Tuna-Kirie <negomatina1996@gmail.com>
2019-12-22 19:20:19 +0300

    init

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+def test_guest_can_go_to_login_page(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    driver.get(link)
+    login_link = driver.find_element_by_css_selector("#login_link")
+    login_link.click()
